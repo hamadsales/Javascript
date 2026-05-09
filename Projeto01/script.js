21bfd05c7cf46cfcf97992ef8cb26f6b0bf7ec1b
Hamad Sales <hamadsales@gmail.com>
2026-05-08 23:14:05 -0300

    "Projeto de calculadora simples com JS"

---
+++ Projeto01/script.js
@@ -0,0 +1,6 @@
+function sum() {
+    window.alert('somou')
+    var zero = window.document.querySelector('input#zero')
+    var one = window.document.querySelector('input#one')
+    window.alert("somou")
+}
